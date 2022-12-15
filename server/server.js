639374961f08621cc1aabf9ed1743bdd762feea0
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-15 14:11:55 +0530

    connected to DB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
+require('colors');
 const express = require("express");
+const connectToDB = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => console.log(`listening to PORT ${PORT}`));
+connectToDB(() => {
+  app.listen(PORT, () => console.log(`listening to PORT ${PORT}`));
+});
+
