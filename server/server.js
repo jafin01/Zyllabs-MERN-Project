3fc0c39964597c7a32707b6e86305277746197be
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-15 22:19:12 +0530

    Added middleware folder and created logger

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ require("dotenv").config();
 require('colors');
 const express = require("express");
 const connectToDB = require("./config/db");
+const logger = require("./middleware/loggerMIddleware");
 
+// App init 
 const app = express();
 
+// MIddleware
+app.use(express.json);
+app.use(logger);
+
+// Connecting DB and listening to PORT
 const PORT = process.env.PORT;
 
 connectToDB(() => {
