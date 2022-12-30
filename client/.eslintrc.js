05fc68465070a66455da87bc0f9b1e13123a7c6e
Jafin Jahfar <jafinjahfar@gmail.com>
2022-12-31 00:42:49 +0530

    eol sequence rule added

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,21 +1,23 @@
 module.exports = {
   env: {
     browser: true,
+
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+
+  extends: ['plugin:react/recommended', 'airbnb'],
+
+  overrides: [],
+
   parserOptions: {
     ecmaVersion: 'latest',
+
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+
+  plugins: ['react'],
+
   rules: {
+    'linebreak-style': ['error', 'windows'],
   },
 };
