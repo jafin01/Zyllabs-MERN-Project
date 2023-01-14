c135e64fc8a1d2de67763b544f88397347649bfd
Jafin Jahfar <jafinjahfar@gmail.com>
2023-01-14 18:37:04 +0530

    completed student crud in school

---
+++ client/src/service/school/student.js
@@ -0,0 +1,74 @@
+import baseUrl from '../../constants/baseUrl';
+
+// Get all students
+export const getAllStudents = async () => {
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWVjYjE1NmY1OGI1ODhjNzA1MWI4NiIsImlhdCI6MTY3MzYwMzUzOSwiZXhwIjoxNjc2MTk1NTM5fQ.CQYvHzQz377s4aZcqub62UGYOfxrNoFecLKxK_fPeHo',
+  });
+
+  try {
+    const response = await fetch(`${baseUrl}/api/school/students/get-all-students`, {
+      method: 'GET',
+      headers: myHeaders,
+    });
+
+    const students = await response.json();
+    return students;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+// Create a student
+export const saveNewStudent = async (student) => {
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWVjYjE1NmY1OGI1ODhjNzA1MWI4NiIsImlhdCI6MTY3MzYwMzUzOSwiZXhwIjoxNjc2MTk1NTM5fQ.CQYvHzQz377s4aZcqub62UGYOfxrNoFecLKxK_fPeHo',
+  });
+
+  const response = await fetch(`${baseUrl}/api/school/students/add-student`, {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify(student),
+  });
+
+  const newStudent = await response.json();
+  return newStudent;
+};
+
+// Update a student
+export const updateStudent = async (student) => {
+  const myHeaders = new Headers({
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWVjYjE1NmY1OGI1ODhjNzA1MWI4NiIsImlhdCI6MTY3MzYwMzUzOSwiZXhwIjoxNjc2MTk1NTM5fQ.CQYvHzQz377s4aZcqub62UGYOfxrNoFecLKxK_fPeHo',
+  });
+
+  const response = await fetch(`${baseUrl}/api/school/students/${student.id}`, {
+    method: 'PATCH',
+    headers: myHeaders,
+    body: JSON.stringify(student),
+  });
+
+  const updatedStudent = await response.json();
+  return updatedStudent;
+};
+
+// Delete a student
+export const deleteStudent = async (id) => {
+  const myHeaders = new Headers({
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYWVjYjE1NmY1OGI1ODhjNzA1MWI4NiIsImlhdCI6MTY3MzYwMzUzOSwiZXhwIjoxNjc2MTk1NTM5fQ.CQYvHzQz377s4aZcqub62UGYOfxrNoFecLKxK_fPeHo',
+  });
+
+  const response = await fetch(`${baseUrl}/api/school/students/${id}`, {
+    method: 'DELETE',
+    headers: myHeaders,
+  });
+
+  const deletedStudent = await response.json();
+  return deletedStudent;
+};
